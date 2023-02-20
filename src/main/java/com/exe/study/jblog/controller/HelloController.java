e63de4f5226a40db01af22a3b37a38c07e1a4ddb
hwaj2 <shinh3222@gmail.com>
2023-02-20 17:14:32 +0900

    feat : 프로퍼티 및 기본설정

---
+++ src/main/java/com/exe/study/jblog/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.exe.study.jblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/html")
+    public String html(){
+        System.out.println("HTML파일이 요청되었습니다.");
+        return "redirect:hello.html";
+    }
+
+    @GetMapping("/image")
+    public String image(){
+        System.out.println("이미지파일이 요청되었습니다.");
+        return "redirect:image/jenny.jpeg";
+    }
+
+    @GetMapping("/jsp")
+    public String jsp(Model model){
+        model.addAttribute("message","안녕^_^");
+        System.out.println("JSP파일이 요청되었습니다.");
+        return "jblog/";
+    }
+
+}
