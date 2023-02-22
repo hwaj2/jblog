3f65e2c6254efa955300a07b284448885e02bdd0
hwaj2 <shinh3222@gmail.com>
2023-02-22 17:07:36 +0900

    feat : 회원 가입 기능

--- src/main/java/com/exe/study/jblog/controller/PostController.java
+++ src/main/java/com/exe/study/jblog/controller/PostController.java
@@ -1,13 +1,36 @@
 package com.exe.study.jblog.controller;
 
+import com.exe.study.jblog.domain.User;
+import com.exe.study.jblog.dto.ResponseDTO;
+import com.exe.study.jblog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping({"","/"})
     public String getPostList(){
         return "index";
+    }
+
+    @GetMapping("/auth/insertUser")
+    public String insertUser(){
+        return "user/insertUser";
+    }
+
+
+    @PostMapping("/auth/insertUser")
+    public @ResponseBody ResponseDTO<?> insertUser(@RequestBody User user){ // ? 인이유는 어떤타입의 데이터가 반환될지 특정할수 없기 때문(문자,객체,컬렉션)
+        userService.insertUser(user);
+        return new ResponseDTO<>(HttpStatus.OK.value(), user.getUsername() + "님의 회원가입이 성공적으로 완료되었습니다.");
     }
 }
---
+++ src/main/java/com/exe/study/jblog/service/UserService.java
@@ -0,0 +1,66 @@
+package com.exe.study.jblog.service;
+
+
+import com.exe.study.jblog.domain.RoleType;
+import com.exe.study.jblog.domain.User;
+import com.exe.study.jblog.persistence.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+   // @Autowired
+    // private PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void insertUser(User user) {
+        user.setRole(RoleType.USER);
+        userRepository.save(user);
+    }
+
+
+/*
+    // 비밀번호를 암호화하여 설정한다.
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        user.setRole(RoleType.USER);
+        if(user.getOauth() == null) {
+        user.setOauth(OAuthType.JBLOG);
+    }
+        userRepository.save(user);
+*/
+
+
+/*
+    @Transactional
+    public User updateUser(User user) {
+        User findUser = userRepository.findById(user.getId()).get();
+        findUser.setUsername(user.getUsername());
+        findUser.setPassword(passwordEncoder.encode(user.getPassword()));
+        findUser.setEmail(user.getEmail());
+
+        return findUser;
+    }
+
+
+
+    @Transactional(readOnly = true)
+    public User getUser(String username) {
+        // 검색 결과가 없다면 빈 객체를 리턴한다.(일반 코드)
+        User findUser = userRepository.findByUsername(username).orElseGet(()->{
+            return new User();
+        });
+
+        return findUser;
+    }
+*/
+
+
+
+}
